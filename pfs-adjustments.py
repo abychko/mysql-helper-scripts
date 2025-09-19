dd8eab1335734cc91ca00526a4f23d21ea9576b5
Alexey Bychko <abychko@gmail.com>
2025-09-19 16:45:26 +0300

    small refactoring

--- pfs-adjustments.py
+++ pfs-adjustments.py
@@ -1,30 +1,37 @@
 #!/usr/bin/env python3
 
-import argparse
+from argparse import ArgumentParser
 from sys import exit
 from os import linesep
+from datetime import datetime
 
-parser = argparse.ArgumentParser(
+parser = ArgumentParser(
                     prog='pfs-adjustments',
                     description='Sets PFS counters from CSV file',
                     epilog='Author: Alexey Bychko <abychko@gmail.com>')
 
-parser.add_argument('--filename', required=True, help='CSV file to process')
-parser.add_argument('--output', help='Output to file', action='store_true')
-parser.add_argument('--output-format', choices=['sql', 'cnf'], help="Output format. SQL or my.cnf snippet")
+parser.add_argument('--infile', required=True, help='CSV file to process')
+parser.add_argument('--format', choices=['sql', 'cnf'], required=True,
+						help="Output format. SQL or my.cnf snippet")
 parser.add_argument('-v', '--verbose', action='store_true')
 
 args = parser.parse_args()
 #
-infile = None
-outfile = None
-outfile_name = None
 lines = []
 #
-def gen_sql_file(ifile=None, ofile=None):
-	if args.verbose:
-		print(f"SQL filename: {outfile_name}")
-	for line in ifile:
+def create_cnf_file(infile=None, outfile=None):
+	for line in infile:
+		if not '/' in line:
+			continue
+
+		name, enabled, _ = line.rstrip().split(',')
+
+		if enabled == "YES":
+			cnf_line = f"performance_schema_instrument = '{name}=ON'"
+			lines.append(cnf_line)
+#
+def create_sql_file(infile=None, outFile=None):
+	for line in infile:
 		if not '/' in line:
 			continue
 		name, enabled, timed = line.rstrip().split(',')
@@ -32,61 +39,52 @@ def gen_sql_file(ifile=None, ofile=None):
 				f"SET ENABLED = '{enabled}', TIMED = '{timed}' "
 				f"WHERE NAME = '{name}';")
 		lines.append(sql_line)
-	for sql_line in lines:
-		if(args.verbose):
-			print(sql_line)
-		ofile.write(sql_line + linesep)
 #
-def gen_cnf_file(ifile=None, ofile=None):
+def create_outfile():
+	now = datetime.now()
+	date_time = now.strftime("%Y-%m-%d %H:%M:%S")
+
+	o_filename = "perfschema." + args.format
+	print(f"=> Creating outfile: {o_filename}")
+# open file
+	try:
+		infile = open(args.infile, 'r')
+	except Exception as err:
+		print(f"==>> Wrong file or file path: {args.infile}!")
+		print(f"{err}")
+		exit(1)
+#
+	try:
+		outfile = open(o_filename, 'w')
+	except Exception as err:
+		print(f"==>> Unable to open outfile: {o_filename}!")
+		print(f"{err}")
+#
 	if args.verbose:
-		print(f"MySQL cnf filename: {outfile_name}")
-
-	ofile.write("#" + linesep + "[mysqld]" + linesep + "#" + linesep)
-	ofile.write("performance_schema_instrument = '%=OFF'" + linesep)
-
-	for line in ifile:
-		if not '/' in line:
-			continue
-		name, enabled, _ = line.rstrip().split(',')
-
-		if enabled == "YES":
-			cnf_line = f"performance_schema_instrument = '{name}=ON'"
-			lines.append(cnf_line)
-
+		print(f"=> CSV filename: {args.infile}")
+		print(f"=> Output format is {args.format.upper()}")
+		print(f"=> Outfile is {o_filename}")
+#
+	if args.format == 'sql':
+		lines.append("--" + linesep + "-- PFS Adjuster " +
+			date_time + linesep + "--")
+		create_sql_file(infile, outfile)
+#
+	if args.format == 'cnf':
+		lines.append("#" + linesep + "## PFS Adjuster " + date_time +
+			linesep + "#" + linesep + "[mysqld]")
+		lines.append("performance_schema_instrument = '%=OFF'")
+		create_cnf_file(infile, outfile)
+#
 	for line in sorted(lines):
 		if(args.verbose):
 			print(line)
-		ofile.write(line + linesep)
+		outfile.write(line + linesep)
 #
-def open_outfile(name=None):
-	try:
-		f = open(outfile_name, 'w')
-	except Exception as err:
-		print(f"==>> Unable to open file: {outfile_name}!")
-		print(f"{err}")
-	return f
 #
-def create_outfile(name=None):
-	outfile = open_outfile(name)
-	gen_cnf_file(infile, outfile)
-#
-try:
-	infile = open(args.filename, 'r')
-except Exception as err:
-	print(f"==>> Wrong file or file path: {args.filename}!")
-	print(f"{err}")
-	exit(1)
-#
-if args.verbose:
-	print(f"CSV filename: {args.filename}")
-#
-if args.output_format == 'sql':
-	outfile_name = args.filename.replace('.csv', '.sql')
-#
-if args.output_format == 'cnf':
-	outfile_name = "perfschema.cnf"
-#
-create_outfile(outfile_name)
+create_outfile()
+
+
 
 
 
